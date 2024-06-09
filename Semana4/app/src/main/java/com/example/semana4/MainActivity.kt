be3c8fba695460828fc6ca245e28900334803ded
machaadolarissa <larissamachaado@hotmail.com>
2024-06-09 12:47:58 -0300

    Semana 4

--- Semana4/app/src/main/java/com/example/semana4/MainActivity.kt
+++ Semana4/app/src/main/java/com/example/semana4/MainActivity.kt
@@ -1,4 +1,61 @@
 package com.example.semana4
 
-class MainActivity {
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.location.LocationManager
+import android.os.Bundle
+import android.view.View
+import android.webkit.WebView
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.core.app.ActivityCompat
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun buscarInformacoesGPS(v: View) {
+        val mLocManager: LocationManager = getSystemService(LOCATION_SERVICE) as LocationManager
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_NETWORK_STATE
+                ),
+                1
+            )
+            return
+        }
+
+        val mLocListener = MinhaLocalizacaoListener()
+
+        mLocManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0f, mLocListener)
+
+        if (mLocManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            val texto = "Latitude: ${MinhaLocalizacaoListener.latitude}\nLongitude: ${MinhaLocalizacaoListener.longitude}\n"
+            Toast.makeText(this, texto, Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(this, "GPS DESABILITADO.", Toast.LENGTH_LONG).show()
+        }
+        mostrarGoogleMaps(MinhaLocalizacaoListener.latitude, MinhaLocalizacaoListener.longitude)
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun mostrarGoogleMaps(latitude: Double, longitude: Double) {
+        val wv: WebView = findViewById(R.id.webv)
+        wv.settings.javaScriptEnabled = true
+        wv.loadUrl("https://www.google.com/maps/search/?api=1&query=$latitude,$longitude")
+    }
 }
